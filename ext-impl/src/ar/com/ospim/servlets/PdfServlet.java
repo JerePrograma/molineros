80c27de55b684a01594a8c40de71c74501da3bed
Jeremías <jeremias.j.riv@gmail.com>
2026-05-14 13:33:27 -0300

    Cambios de librerias y jasper

--- ext-impl/src/ar/com/ospim/servlets/PdfServlet.java
+++ ext-impl/src/ar/com/ospim/servlets/PdfServlet.java
@@ -282,7 +282,7 @@ public class PdfServlet extends HttpServlet {
 		HashMap<String, String> hm = new HashMap<String, String>();
 		//hm.put("id_ini", id_orden_pagoIni);		
 		hm.put("ID_ORDEN_PAGO", id_orden_pagoIni);
-        hm.put("SUBREPORT_DIR", "jasper/orden_pago/");
+        hm.put("SUBREPORT_DIR", "jasper/");
 		hm.put("pathimage", "jasper/firma_carolina.jpg");
 		crearPdf(req, res, ORDEN_PAGO_OSPIM, hm, ORDEN_PAGO_OSPIM_PDF_FILENAME);
 	}
