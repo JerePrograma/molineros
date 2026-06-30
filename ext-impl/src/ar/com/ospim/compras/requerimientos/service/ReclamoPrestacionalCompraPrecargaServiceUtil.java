c3aca6d2cfa8adbf899f50a3f3a5d1e3331e8702
Jeremías <95102411+JerePrograma@users.noreply.github.com>
2026-06-30 13:04:51 -0300

    feat(compras): agregar precarga de reclamo prestacional

---
+++ ext-impl/src/ar/com/ospim/compras/requerimientos/service/ReclamoPrestacionalCompraPrecargaServiceUtil.java
@@ -0,0 +1,6 @@
+package ar.com.ospim.compras.requerimientos.service;
+
+public final class ReclamoPrestacionalCompraPrecargaServiceUtil {
+    private ReclamoPrestacionalCompraPrecargaServiceUtil() {
+    }
+}
