688357807720e3ffec1aa941c78fa289d3369bcb
Jeremías <95102411+JerePrograma@users.noreply.github.com>
2026-07-22 23:03:18 -0300

    fix(reclamos): agrega soporte SUR para altas desde Compras

---
+++ ext-web/docroot/html/portlet/autorizaciones/reclamos_prestacionales/view_reclamo_compras_surge_patch.js
@@ -0,0 +1,247 @@
+(function(window, jQuery) {
+    if (!jQuery) {
+        return;
+    }
+
+    function instalar() {
+        var config = window.ReclamoPrestacionalViewConfig || {};
+        var namespace = config.namespace || "";
+        var inicial = jQuery(
+                "#" + namespace + "recuperable_sur_compra_inicial"
+        );
+
+        if (!inicial.length
+                || jQuery.fn.__rpComprasSurInstalado) {
+            return;
+        }
+
+        var valorActual = inicial.val() === "1" ? "1" : "0";
+
+        function normalizarValor(valor) {
+            var numero = parseInt(valor, 10);
+
+            return numero >= 0 && numero <= 3
+                    ? String(numero)
+                    : "0";
+        }
+
+        function esEndpointPrestacion(url) {
+            var texto = String(url || "");
+
+            return texto.indexOf(
+                    "struts_action=/autorizaciones/"
+                            + "lista_prestaciones_reclamos"
+            ) >= 0
+                    || texto.indexOf(
+                            "struts_action=/autorizaciones/"
+                                    + "editar_reclamosprestaciones"
+                    ) >= 0;
+        }
+
+        function corregirUrl(url) {
+            var texto = String(url || "");
+            var patron = /([?&])recuperableSur=[^&]*/;
+
+            if (!esEndpointPrestacion(texto)) {
+                return url;
+            }
+
+            if (patron.test(texto)) {
+                return texto.replace(
+                        patron,
+                        "$1recuperableSur=" + valorActual
+                );
+            }
+
+            return texto
+                    + (texto.indexOf("?") >= 0 ? "&" : "?")
+                    + "recuperableSur="
+                    + valorActual;
+        }
+
+        function corregirDatos(datos) {
+            var copia;
+
+            if (datos == null) {
+                copia = {};
+            } else if (typeof datos === "object") {
+                copia = jQuery.extend({}, datos);
+            } else {
+                return datos;
+            }
+
+            copia.recuperableSur = parseInt(valorActual, 10);
+
+            return copia;
+        }
+
+        function envolverCallback(callback) {
+            if (typeof callback !== "function") {
+                return callback;
+            }
+
+            return function() {
+                var resultado = callback.apply(this, arguments);
+
+                window.setTimeout(aplicarSelector, 0);
+
+                return resultado;
+            };
+        }
+
+        function registrarCambios(selector) {
+            selector.each(function() {
+                var control = jQuery(this);
+
+                if (control.data("rpComprasSurCambio")) {
+                    return;
+                }
+
+                control.data("rpComprasSurCambio", true);
+                control.change(function() {
+                    valorActual = normalizarValor(
+                            jQuery(this).val()
+                    );
+                });
+            });
+        }
+
+        function aplicarSelector() {
+            var alta = jQuery(
+                    "#" + namespace + "recuperable_sur"
+            );
+            var edicion = jQuery(
+                    "#" + namespace + "recuperable_surEdicion"
+            );
+
+            registrarCambios(alta);
+            registrarCambios(edicion);
+
+            alta.removeAttr("disabled").val(valorActual);
+            edicion.removeAttr("disabled").val(valorActual);
+
+            jQuery(
+                    "#" + namespace + "datos_edicion_prestacion"
+            ).hide();
+            jQuery(
+                    "#" + namespace + "datos_prestacion_ingreso"
+            ).show();
+
+            if (typeof window.cambiorecuperable === "function") {
+                window.cambiorecuperable();
+            }
+
+            if (typeof window.cambiorecuperableEdicion === "function") {
+                window.cambiorecuperableEdicion();
+            }
+        }
+
+        function envolverLoad(loadOriginal) {
+            if (typeof loadOriginal !== "function"
+                    || loadOriginal.__rpComprasSur) {
+                return loadOriginal;
+            }
+
+            var loadCompras = function(url, datos, callback) {
+                var urlCorregida = corregirUrl(url);
+                var datosCorregidos = datos;
+                var callbackCorregido = callback;
+
+                if (typeof datos === "function") {
+                    callbackCorregido = datos;
+                    datosCorregidos = undefined;
+                }
+
+                if (esEndpointPrestacion(urlCorregida)) {
+                    datosCorregidos = corregirDatos(datosCorregidos);
+                    callbackCorregido = envolverCallback(
+                            callbackCorregido
+                    );
+                }
+
+                if (datosCorregidos === undefined) {
+                    return loadOriginal.call(
+                            this,
+                            urlCorregida,
+                            callbackCorregido
+                    );
+                }
+
+                return loadOriginal.call(
+                        this,
+                        urlCorregida,
+                        datosCorregidos,
+                        callbackCorregido
+                );
+            };
+
+            loadCompras.__rpComprasSur = true;
+            loadCompras.__rpComprasSurOriginal = loadOriginal;
+
+            return loadCompras;
+        }
+
+        var ajaxOriginal = jQuery.ajax;
+
+        if (typeof ajaxOriginal === "function"
+                && !ajaxOriginal.__rpComprasSur) {
+
+            var ajaxCompras = function(opciones) {
+                var args = arguments;
+                var configuracion;
+
+                if (arguments.length === 1
+                        && opciones
+                        && typeof opciones === "object"
+                        && esEndpointPrestacion(opciones.url)) {
+
+                    configuracion = jQuery.extend({}, opciones);
+                    configuracion.url = corregirUrl(
+                            configuracion.url
+                    );
+                    configuracion.data = corregirDatos(
+                            configuracion.data
+                    );
+                    configuracion.complete = envolverCallback(
+                            configuracion.complete
+                    );
+
+                    return ajaxOriginal.call(
+                            this,
+                            configuracion
+                    );
+                }
+
+                return ajaxOriginal.apply(this, args);
+            };
+
+            ajaxCompras.__rpComprasSur = true;
+            ajaxCompras.__rpComprasSurOriginal = ajaxOriginal;
+            jQuery.ajax = ajaxCompras;
+        }
+
+        jQuery.fn.load = envolverLoad(jQuery.fn.load);
+
+        if (typeof window.ReclamoPrestacionalJQueryLoadOriginal
+                === "function") {
+
+            window.ReclamoPrestacionalJQueryLoadOriginal =
+                    envolverLoad(
+                            window.ReclamoPrestacionalJQueryLoadOriginal
+                    );
+        }
+
+        jQuery.fn.__rpComprasSurInstalado = true;
+
+        jQuery(document).ajaxComplete(function() {
+            window.setTimeout(aplicarSelector, 0);
+        });
+
+        aplicarSelector();
+        window.setTimeout(aplicarSelector, 0);
+    }
+
+    jQuery(function() {
+        window.setTimeout(instalar, 0);
+    });
+})(window, window.jQuery);
